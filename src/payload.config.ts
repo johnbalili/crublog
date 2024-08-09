dfa761fe6bd9e6b8d94a6e8c349d393354dff584
johnbalili <31870373+johnbalili@users.noreply.github.com>
2024-08-09 13:05:49 +0800

    Initial Setup

---
+++ src/payload.config.ts
@@ -0,0 +1,122 @@
+import path from 'path'
+
+import { payloadCloud } from '@payloadcms/plugin-cloud'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { slateEditor } from '@payloadcms/richtext-slate'
+import { buildConfig } from 'payload/config'
+import {
+  lexicalEditor
+} from '@payloadcms/richtext-lexical'
+
+import Users from './collections/Users'
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    bundler: webpackBundler(),
+  },
+  editor: slateEditor({}),
+  collections: [
+    {
+      slug: 'posts',
+      fields:[
+        {
+        name: 'title',
+        type: 'text',
+        required: true,
+        },
+        {
+          name: 'slug',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'author',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'position',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'author_image',
+          type: 'upload',
+          relationTo: 'media',
+        },
+        {
+          name: 'category',
+          type: 'relationship',
+          relationTo: 'categories',
+          label: 'title',
+        },
+        {
+          name: 'date',
+          type: 'date',
+        },
+        {
+          name: 'content',
+          type: 'richText',
+          editor: lexicalEditor(),
+        },
+        {
+          name: 'excerpt',
+          type: 'textarea',
+        },
+        {
+          name: 'featured_image',
+          type: 'upload',
+          relationTo: 'media'
+        }
+      ]
+    },
+    { 
+      slug: 'media',
+      fields: [
+        {
+          name: 'alt',
+          type: 'text',
+        }
+      ],
+      upload: true
+    },
+    {
+      slug: 'categories',
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+        },
+        {
+          name: 'slug',
+          type: 'text',
+        },
+        {
+          name: 'image',
+          type: 'upload',
+          relationTo: 'media',
+        }
+      ],
+    },
+    Users
+  ],
+  upload: {
+    limits: {
+      fileSize: 2000000,
+    }
+  },
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+  plugins: [payloadCloud()],
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI,
+    },
+  }),
+})
